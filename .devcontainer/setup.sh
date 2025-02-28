67ece5f0f7621668e26dbf65b647a1511b7244cb
Quanyi Ma <eli@patch.sh>
2025-02-28 22:11:49 +0800

    Update the devcontainer config
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -20,17 +20,16 @@ apt-get install -y \
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
 rustup install default
 rustup component add rustfmt
 rustup component add clippy
-source $HOME/.cargo/env
 cargo install cargo-expand
 cargo install cargo-edit
 
-## Install Buck2 and Reindeer
+## Install Buck2
 wget https://github.com/facebook/buck2/releases/download/2025-02-01/buck2-x86_64-unknown-linux-musl.zst
 zstd -d /home/buck2-x86_64-unknown-linux-musl.zst
 mv /home/buck2-x86_64-unknown-linux-musl /home/buck2
 chmod +x /home/buck2
 mv /home/buck2 /usr/local/bin/buck2
-cargo install --locked --git https://github.com/facebookincubator/reindeer reindeer
